b574fdd420d6dbccf1fcafa22399e04e9027ddb3
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-09-14 14:11:25 +0200

    formatting

--- src/main/java/com/mytests/micronaut/controllers/MyController.java
+++ src/main/java/com/mytests/micronaut/controllers/MyController.java
@@ -30,10 +30,12 @@ public class MyController {
     public void setup() {
         setupDBService.setUp();
     }
+
     @Post("/add/{fname}/{sname}/{year}")
     public void addPerson(@PathVariable String fname, @PathVariable String sname, @PathVariable String year) {
-        setupDBService.addSinglePerson(fname,sname,Integer.parseInt(year));
+        setupDBService.addSinglePerson(fname, sname, Integer.parseInt(year));
     }
+
     @Get("/all")
     public List<Person> listAll() {
         return personService.listAll();
@@ -73,18 +75,19 @@ public class MyController {
     public List<Person> findByName1() {
         return personService.testFindByNameStandard();
     }
+
     @Get("/byName2")
     public List<Person> findByName2() {
         return personService.testFindByNameCustom();
     }
 
     @Get("/byCards1")
-    public List<Person> findByCardNumberPattern(){
+    public List<Person> findByCardNumberPattern() {
         return personService.testFindByCards();
     }
 
     @Get("/issuesAfter")
-    public List<Issue> getIssuesCreatedAfter(){
+    public List<Issue> getIssuesCreatedAfter() {
         return issueService.findByCreatedAfter();
     }
 
